fcc8aa45968228f686db0824533e2dab2ebb413c
Alexey Borisov <a.borisov@fun.co>
2023-08-29 00:16:52 +0300

    some key binds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,9 @@ local opts = { noremap = true, silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
-
 keymap("n", "<leader>ti", ":!terraform init<CR>", opts)
 keymap("n", "<leader>tv", ":!terraform validate<CR>", opts)
 keymap("n", "<leader>tp", ":!terraform plan<CR>", opts)
 keymap("n", "<leader>taa", ":!terraform apply -auto-approve<CR>", opts)
+keymap("n", "<C-d>", "<C-d>zz", { noremap = true })
+keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
